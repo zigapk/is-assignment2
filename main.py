e4333f295cf9b42548bbd6462e194184b0110924
Žiga Patačko Koderman <ziga@zerodays.dev>
2021-12-28 13:11:28 +0100

    plot word frequencies

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
 import pandas as pd
-import nltk
-# nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.stem import SnowballStemmer
 import re
 from nltk.tokenize import word_tokenize
-
-df = pd.read_table('data/train_data.tsv')
-
-# Convert to lower case.
-df['text_a'] = df['text_a'].apply(lambda x: x.lower())
-
-# Remove urls.
-remove_urls = lambda x: re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', x)
-df['text_a'] = df['text_a'].apply(remove_urls)
-
-# Remove punctuations and numbers.
-PUNCTUATIONS_AND_NUMBERS = '!()-[]{};:\'"\\,<>./?@#$%^&*_~0123456789'
-remove_punctuations = lambda x: ''.join([i for i in x if i not in PUNCTUATIONS_AND_NUMBERS])
-df['text_a'] = df['text_a'].apply(remove_punctuations)
-
-# Remove stopwords.
-ENGLISH_STOPWORDS = set(stopwords.words('english'))
-remove_stopwords = lambda x: ' '.join([w for w in x.split() if w not in ENGLISH_STOPWORDS])
-df['text_a'] = df['text_a'].apply(remove_stopwords)
+from sklearn.feature_extraction.text import CountVectorizer
+import matplotlib.pyplot as plt
 
 
-print(df)
+def load_and_transform(table_name):
+    df = pd.read_table(table_name)
 
-# Stem words.
-stemmer = SnowballStemmer('english')
-stem_words = lambda x: ' '.join(stemmer.stem(token) for token in word_tokenize(x))
-df['text_a'] = df['text_a'].apply(stem_words)
+    # Convert to lower case.
+    df['text_a'] = df['text_a'].apply(lambda x: x.lower())
 
-# Strip.
-df['text_a'] = df['text_a'].apply(lambda x: (' '.join(x.split())).strip())
-print(df)
+    # Remove urls.
+    remove_urls = lambda x: re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', x)
+    df['text_a'] = df['text_a'].apply(remove_urls)
+
+    # Remove non alpha and not ascii chars.
+    remove_non_alpha = lambda x: ''.join([i for i in x if (i.isalpha() and ord(i) < 256) or i == ' '])
+    df['text_a'] = df['text_a'].apply(remove_non_alpha)
+    print(df)
+
+    # Remove stopwords.
+    ENGLISH_STOPWORDS = set(stopwords.words('english'))
+    remove_stopwords = lambda x: ' '.join([w for w in x.split() if w not in ENGLISH_STOPWORDS])
+    df['text_a'] = df['text_a'].apply(remove_stopwords)
+
+    # Stem words.
+    stemmer = SnowballStemmer('english')
+    stem_words = lambda x: ' '.join(stemmer.stem(token) for token in word_tokenize(x))
+    df['text_a'] = df['text_a'].apply(stem_words)
+
+    # Strip.
+    df['text_a'] = df['text_a'].apply(lambda x: (' '.join(x.split())).strip())
+    return df
+
+
+df = load_and_transform('data/train_data.tsv')
+vec = CountVectorizer()
+X = vec.fit_transform(df['text_a'])
+td_matrix = pd.DataFrame(X.toarray(), columns=vec.get_feature_names_out())
+
+td_matrix = td_matrix.T
+td_matrix['total_count'] = td_matrix.sum(axis=1)
+td_matrix = td_matrix.sort_values(by='total_count', ascending=False)[:35]
+print(td_matrix)
+
+td_matrix['total_count'].plot.bar()
+plt.show()
